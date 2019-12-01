ddb4b73533c88ea5e350e9e85bd4147261d6a274
Johnson Zhang <zhang_xihan@hotmail.com>
2019-11-30 22:59:15 -0600

    Created user class

---
+++ users.py
@@ -0,0 +1,10 @@
+class users:
+    def __init__(self, id, first_name, last_name, nick_name, venmo_address, password, is_admin, register_date):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.nick_name = nick_name
+        self.venmo_address = venmo_address
+        self.password = password
+        self.is_admin = is_admin
+        self.register_date = register_date
